89405ac72375f858fa2c575f927e913872959dd5
Ricardo Bustamante <ricardo.bustamante@tapps.com.br>
2019-01-26 23:45:17 -0200

    Add all

---
+++ Assets/Game/Scripts/MusicManager.cs
@@ -0,0 +1,63 @@
+namespace Game.Scripts
+{
+    using UnityEngine;
+
+    public class MusicManager : MonoBehaviour
+    {
+        public bool PlayOnAwake = true;
+        
+        public AudioSource musicIntro;
+        public AudioSource musicLoop;
+
+        public void SetMusic(AudioClip intro, AudioClip loop)
+        {
+            musicIntro.clip = intro;
+            musicLoop.clip = loop;
+        }
+
+        public void StopMusic()
+        {
+            musicIntro.Stop();
+            musicLoop.Stop();
+        }
+
+        public float GetVolume()
+        {
+            return musicLoop.volume;
+        }
+
+        public void SetVolume(float volume)
+        {
+            musicIntro.volume = volume;
+            musicLoop.volume = volume;
+        }
+
+        public void PlayMusic()
+        {
+            if (musicIntro.clip != null)
+            {
+                musicIntro.Play();
+                if (musicLoop.clip != null)
+                {
+                    musicLoop.PlayDelayed(musicIntro.clip.length / Mathf.Abs(musicIntro.pitch));
+                }
+            }
+            else if (musicLoop.clip != null)
+            {
+                musicLoop.Play();
+            }
+            else
+            {
+                Debug.LogError("Music is null!");
+            }
+        }
+
+        private void Start()
+        {
+            if (PlayOnAwake)
+            {
+                PlayMusic();
+            }
+        }
+    }
+}
