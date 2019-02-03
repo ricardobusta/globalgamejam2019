67f7b0623bcecaf0a5f716209100cbbc213ac795
Ricardo Bustamante <ricardobqueiroz@gmail.com>
2019-02-02 23:30:07 -0200

    Refining stuff. Bugfix.

--- Assets/Game/Scripts/Cinematic.cs
+++ Assets/Game/Scripts/Cinematic.cs
@@ -1,13 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using DG.Tweening;
+﻿using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Cinematic : MonoBehaviour {
     public Image BlackScreen;
-    public Image GGJLogo;
+    public CanvasGroup GGJLogo;
     public GameObject Enemy;
     public GameObject[] DisableStuff;
 
@@ -16,53 +14,54 @@ public class Cinematic : MonoBehaviour {
         BlackScreen.gameObject.SetActive(true);
         Enemy.gameObject.SetActive(false);
 
-        GGJLogo.color = new Color(1, 1, 1, 0);
+        GGJLogo.alpha = 0;
 
         // DO NOT TRY THIS AT HOME! GAME JAM STUFF ONLY
 
         // Delay
-        DOVirtual.Float(0, 0, 1, t => { })
+        DOVirtual.DelayedCall(1,
             // Show
-            .onComplete += () =>
-            DOVirtual.Float(0, 1, 1, t => { GGJLogo.color = new Color(1, 1, 1, t); })
-                // Hide
-                .onComplete += () =>
-                DOVirtual.Float(1, 0, 1, t => { GGJLogo.color = new Color(1, 1, 1, t); })
-                    // Hide black screen
-                    .onComplete += () => {
-                    DOVirtual.Float(1, 0, 1, t => { BlackScreen.color = new Color(0, 0, 0, t); })
-                            // Wait
-                            .onComplete +=
-                        () => {
-                            DOVirtual.Float(0, 0, 1, t => { })
-                                // Enemy Attack
-                                .onComplete += () => {
-                                Enemy.gameObject.SetActive(true);
-                                DOVirtual.Float(-6, -1, 0.5f,
-                                        t => { Enemy.transform.position = new Vector3(t, 0.4f, 0); })
-                                    // Steal stuff
-                                    .SetEase(Ease.Linear)
+            () => {
+                DOVirtual.Float(0, 1, 1, t => { GGJLogo.alpha = t; })
+                    // Hide
+                    .onComplete += () => DOVirtual.DelayedCall(0.5f, () =>
+                    DOVirtual.Float(1, 0, 1, t => { GGJLogo.alpha = t; })
+                        // Hide black screen
+                        .onComplete += () => {
+                        DOVirtual.Float(1, 0, 1, t => { BlackScreen.color = new Color(0, 0, 0, t); })
+                                // Wait
+                                .onComplete +=
+                            () => {
+                                DOVirtual.Float(0, 0, 1, t => { })
+                                    // Enemy Attack
                                     .onComplete += () => {
-                                    DOVirtual.Float(0, 1, 0.1f, t => { BlackScreen.color = new Color(1, 1, 1, t); })
+                                    Enemy.gameObject.SetActive(true);
+                                    DOVirtual.Float(-6, -1, 0.5f,
+                                            t => { Enemy.transform.position = new Vector3(t, 0.4f, 0); })
+                                        // Steal stuff
+                                        .SetEase(Ease.Linear)
                                         .onComplete += () => {
-                                        foreach (var stuff in DisableStuff) {
-                                            stuff.SetActive(false);
-                                        }
+                                        DOVirtual.Float(0, 1, 0.1f, t => { BlackScreen.color = new Color(1, 1, 1, t); })
+                                            .onComplete += () => {
+                                            foreach (var stuff in DisableStuff) {
+                                                stuff.SetActive(false);
+                                            }
 
-                                        DOVirtual.Float(1, 0, 0.1f,
-                                            t => { BlackScreen.color = new Color(1, 1, 1, t); });
-                                    };
-                                    // Get out
-                                    DOVirtual.Float(-1, 6, 0.5f,
-                                                t => { Enemy.transform.position = new Vector3(t, 0.4f, 0); })
-                                            .SetEase(Ease.Linear)
-                                            .onComplete +=
-                                        () => {
-                                            DOVirtual.DelayedCall(1, () => { SceneManager.LoadScene("Stage1"); });
+                                            DOVirtual.Float(1, 0, 0.1f,
+                                                t => { BlackScreen.color = new Color(1, 1, 1, t); });
                                         };
+                                        // Get out
+                                        DOVirtual.Float(-1, 6, 0.5f,
+                                                    t => { Enemy.transform.position = new Vector3(t, 0.4f, 0); })
+                                                .SetEase(Ease.Linear)
+                                                .onComplete +=
+                                            () => {
+                                                DOVirtual.DelayedCall(1, () => { SceneManager.LoadScene("Stage1"); });
+                                            };
+                                    };
                                 };
                             };
-                        };
-                };
+                    });
+            });
     }
 }
--- Assets/Game/Scripts/CrabController.cs
+++ Assets/Game/Scripts/CrabController.cs
@@ -1,12 +1,10 @@
 ﻿using UnityEngine;
 
-namespace Game.Scripts
-{
+namespace Game.Scripts {
     using System;
 
     [RequireComponent(typeof(Rigidbody2D), typeof(Animator))]
-    public class CrabController : MonoBehaviour
-    {
+    public class CrabController : MonoBehaviour {
         public Cooldown punchCooldown;
 
         private float punchRemainingCooldown;
@@ -19,8 +17,11 @@ namespace Game.Scripts
         public Shell Shell;
         public Transform ShellAnchor;
 
-        [NaughtyAttributes.ReadOnly] public bool hasShell;
-        [NaughtyAttributes.ReadOnly] public bool Grounded = false;
+        [NaughtyAttributes.ReadOnly]
+        public bool hasShell;
+
+        [NaughtyAttributes.ReadOnly]
+        public bool Grounded = false;
 
         private Rigidbody2D body;
         private Animator animator;
@@ -35,37 +36,25 @@ namespace Game.Scripts
 
         public Rigidbody2D Anemone;
 
-        public void Attack()
-        {
-            if (punchCooldown.Trigger())
-            {
+        public void Attack() {
+            if (hasShell) {
+                if (Shell.SpecialCooldown.Trigger()) {
+                    InvokeSpecial(Shell.Type);
+                }
+            } else if (punchCooldown.Trigger()) {
                 animator.SetTrigger("Attack");
             }
         }
 
-        public void Special()
-        {
-            if (hasShell)
-            {
-                if (Shell.SpecialCooldown.Trigger())
-                {
-                    InvokeSpecial(Shell.Type);
-                }
-            }
-        }
-
-        public void ThrowAnemone()
-        {
+        public void ThrowAnemone() {
             SFXManager.PlaySound(SFXManager.SFX.throwAnemone);
             var anemone = Instantiate(Anemone, transform.position, Quaternion.identity);
             anemone.velocity = new Vector2(transform.localScale.x * 2, 4);
         }
 
-        private void InvokeSpecial(Shell.ShellType shellType)
-        {
+        private void InvokeSpecial(Shell.ShellType shellType) {
             Debug.Log("Special");
-            switch (shellType)
-            {
+            switch (shellType) {
                 case Shell.ShellType.Basic:
                     break;
                 case Shell.ShellType.Helmet:
@@ -77,37 +66,30 @@ namespace Game.Scripts
                     break;
                 case Shell.ShellType.Lamp:
                     animator.SetTrigger("LampSpecial");
-                    dashRemaining = 5.0f/6.0f;
+                    dashRemaining = 5.0f / 6.0f;
                     break;
                 default:
                     throw new ArgumentOutOfRangeException(nameof(shellType), shellType, null);
             }
         }
 
-        public void Handle(float horizontalInput, float verticalInput)
-        {
+        public void Handle(float horizontalInput, float verticalInput) {
             var horizontalSpeed = horizontalInput * Speed;
             var jump = verticalInput * JumpSpeed;
 
             float verticalSpeed = 0;
 
-            if (dashRemaining <= 0)
-            {
+            if (dashRemaining <= 0) {
                 body.gravityScale = 1;
-                if (Grounded && jump > 0)
-                {
+                if (Grounded && jump > 0) {
                     verticalSpeed = jump;
                     timeSinceJump = 0;
                     Grounded = false;
                     PlayJumpStart();
-                }
-                else
-                {
+                } else {
                     verticalSpeed = body.velocity.y;
                 }
-            }
-            else
-            {
+            } else {
                 body.gravityScale = 0;
                 horizontalSpeed = transform.localScale.x * Speed * 2;
                 verticalSpeed = 0;
@@ -117,74 +99,59 @@ namespace Game.Scripts
 
             body.velocity = new Vector2(horizontalSpeed, verticalSpeed);
             animator.SetFloat("Speed", Mathf.Abs(horizontalSpeed));
-            if (Math.Abs(horizontalInput) > 0.1f)
-            {
+            if (Math.Abs(horizontalInput) > 0.1f) {
                 transform.localScale = new Vector3(horizontalInput, 1, 1);
             }
         }
 
-        public void PlayHelmetWind()
-        {
+        public void PlayHelmetWind() {
             SFXManager.PlaySound(SFXManager.SFX.helmetWind);
         }
 
-        public void PlayHelmetHit()
-        {
+        public void PlayHelmetHit() {
             SFXManager.PlaySound(SFXManager.SFX.helmetHit);
         }
 
-        public void PlayPunchWind()
-        {
+        public void PlayPunchWind() {
             SFXManager.PlaySound(SFXManager.SFX.punchWind);
         }
 
-        public void PlayJumpStart()
-        {
+        public void PlayJumpStart() {
             SFXManager.PlaySound(SFXManager.SFX.jump);
         }
 
-        public void PlayJumpEnd()
-        {
+        public void PlayJumpEnd() {
             SFXManager.PlaySound(SFXManager.SFX.land);
         }
 
-        public void PlayDashEletric()
-        {
+        public void PlayDashEletric() {
             SFXManager.PlaySound(SFXManager.SFX.dashEletric);
         }
 
-        private void Awake()
-        {
+        private void Awake() {
             body = GetComponent<Rigidbody2D>();
             hasShell = Shell != null;
             animator = GetComponent<Animator>();
         }
 
-        private void OnCollisionEnter2D(Collision2D other)
-        {
+        private void OnCollisionEnter2D(Collision2D other) {
             HandleCollision(other);
         }
 
-        private void OnCollisionStay2D(Collision2D other)
-        {
+        private void OnCollisionStay2D(Collision2D other) {
             HandleCollision(other);
         }
 
-        private void HandleCollision(Collision2D other)
-        {
-            switch (other.collider.gameObject.layer)
-            {
+        private void HandleCollision(Collision2D other) {
+            switch (other.collider.gameObject.layer) {
                 case GameConstants.GroundLayer:
-                    if (Grounded || timeSinceJump < 0.5f)
-                    {
+                    if (Grounded || timeSinceJump < 0.5f) {
                         return;
                     }
 
                     Debug.Log("Collide with ground");
-                    foreach (var contact in other.contacts)
-                    {
-                        if (contact.normal == Vector2.up)
-                        {
+                    foreach (var contact in other.contacts) {
+                        if (contact.normal == Vector2.up) {
                             Grounded = true;
                             PlayJumpEnd();
                             return;
@@ -203,17 +170,14 @@ namespace Game.Scripts
             }
         }
 
-        private void SetShell(Shell shell)
-        {
+        private void SetShell(Shell shell) {
             RemoveShell();
 
             SFXManager.PlaySound(SFXManager.SFX.collectShell);
             shell.Activate(ShellAnchor,
-                () =>
-                {
+                () => {
                     hasShell = true;
-                    if (Shell != null)
-                    {
+                    if (Shell != null) {
                         Shell.Deactivate();
                     }
 
@@ -221,36 +185,30 @@ namespace Game.Scripts
                 });
         }
 
-        private void RemoveShell()
-        {
-            if (hasShell)
-            {
+        private void RemoveShell() {
+            if (hasShell) {
                 hasShell = false;
                 Shell.Deactivate();
                 Shell = null;
             }
         }
 
-        public void Die()
-        {
+        public void Die() {
             Died?.Invoke();
             RemoveShell();
             gameObject.SetActive(false);
         }
 
-        private void Update()
-        {
+        private void Update() {
             punchCooldown.Update(Time.deltaTime);
 
             timeSinceJump += Time.deltaTime;
 
-            if (dashRemaining > 0)
-            {
+            if (dashRemaining > 0) {
                 dashRemaining -= Time.deltaTime;
             }
 
-            if (hasShell)
-            {
+            if (hasShell) {
                 var t = Shell.transform;
                 t.position = ShellAnchor.position;
                 t.rotation = ShellAnchor.rotation;
